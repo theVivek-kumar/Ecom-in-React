da14f294e7cd20524184c5144a91614540ca0a99
Vivek-jpg <kumarvivekazio@gmail.com>
2022-03-16 02:17:39 +0530

    feat

---
+++ ecom-app/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const navbar = () => {
+    return <nav className='navbar'>
+        <div className='navbar-logo' >
+            XOXOSHOP
+        </div>
+        <div className='navabr-login' > Login</div>
+        <div className='navabr-login' > Signin</div>
+        
+  </nav>
+};
+export default navbar;
