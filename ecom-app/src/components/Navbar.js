a4e59de41db33c688b5a59ba752802232e7947c9
Vivek-jpg <kumarvivekazio@gmail.com>
2022-03-21 23:42:29 +0530

    feat

--- ecom-app/src/components/Navbar.js
+++ ecom-app/src/components/Navbar.js
@@ -2,12 +2,21 @@ import React from 'react';
 
 const navbar = () => {
     return <nav className='navbar'>
-        <div className='navbar-logo' >
-            XOXOSHOP
-        </div>
-        <div className='navabr-login' > Login</div>
-        <div className='navabr-login' > Signin</div>
-        
+   <div className="avatar-header1">
+    <div className="container">
+      <div className="logo-img">
+         
+          <div className="logo-text">FATISH UI</div>
+      </div>
+
+          <div className="text">
+          <div className="home"><a class="text" href="#">HOME</a></div>
+          <div className="home"><a class="text" href="#">About</a></div>
+      </div>
+
+    </div>
+
+</div>
   </nav>
 };
 export default navbar;
