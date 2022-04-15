39cc20edfdb40aae445241b307583d48c68d7d91
Vivek-jpg <57774129+theVivek-kumar@users.noreply.github.com>
2022-04-15 16:58:20 +0530

    fix-root-folder

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,19 +7,19 @@ const Footer = () => {
                 <div className='footer-contact one'>CONTACT</div>
                 <div className='footer-contact two' >1234-3456-5678</div>
                 <div className='footer-contact three'>FASHION SHOP</div>
-            </div>
+             </div>
              <div className='footer-container-table' >
                 <div  className='footer-contact one'>E-MAIL US</div>
                 <div  className='footer-contact two'>1234GYM@gmail.com</div>
                 <div  className='footer-contact three'>facebook
                     instagram
                 </div>
-            </div>
+             </div>
              <div className='footer-container-table' >
                 <div className='footer-contact one'>Get notified for our secret scale!</div>
                  <div className='navbar-searchbox-container'>
                 <input className='search-input' placeholder=' Search for procuct,and more...'  ></input><BiSearch className='search-icon' />
-            </div>
+                </div>
                 <div>
                     <ul className='footer-list'>
                         <li>
