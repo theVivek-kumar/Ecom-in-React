0e47940cd1da3e71895397e5cb8b37559fe3c211
Vivek kumar <57774129+theVivek-kumar@users.noreply.github.com>
2022-09-15 04:04:22 +0530

    fix-ui-checktailwind

--- src/components/herosection/CardComponent.js
+++ src/components/herosection/CardComponent.js
@@ -9,7 +9,7 @@ const CardComponent = () => {
                     </div>
                     <div className="hero-btn-section" >
                     <NavLink to ="/Product">GET STARTED</NavLink>
-                    <a className="Secondary-btn" href="/my-cart/wishlist/wishlist.html">BUY NOW</a>
+                    <a class="bg-blue-100  text-white font-bold py-2 px-4 rounded" href="/my-cart/wishlist/wishlist.html">BUY NOW</a>
                     </div> 
                     </div>  
                     <div className="hero-img"><img src="https://img.freepik.com/free-photo/friendly-smiling-brunette-woman-ready-help-assist-holding-hands-together-looking-pleasant-standing-t-shirt-against-white-background_176420-45398.jpg?t=st=1648060035~exp=1648060635~hmac=bc222cc9ccee2dc37bb3c18f053ec07f7c305f67bc07a20c526722da3ac9932cw=900" className="hero-img-head"
